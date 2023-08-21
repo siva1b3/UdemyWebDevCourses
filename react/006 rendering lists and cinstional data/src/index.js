04142ad78379a281ff95070f9468d182ca9bbb2d
siva1b3 <siva1b3@gmail.com>
2023-08-21 11:51:04 +0530

    006

---
+++ react/006 rendering lists and cinstional data/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import App01 from "./01LastModuleCode/App";
+import App02 from "./02RenderingListsusingmap/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    {/* Last module code */}
+    {/* <App01 /> */}
+
+    {/* rendering data using map method */}
+    <App02 />
+  </StrictMode>
+);
