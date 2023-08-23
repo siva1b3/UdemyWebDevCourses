8e5824bfcbbc301c9400b8660db2804b575d35e3
siva1b3 <siva1b3@gmail.com>
2023-08-23 11:13:36 +0530

    Changes to be committed: 	new file:   react/006 rendering lists and cinstional data/src/06ConditionalRendering/App.js 	new file:   react/006 rendering lists and cinstional data/src/06ConditionalRendering/components/Expenses/ExpenseDate.css 	new file:   react/006 rendering lists and cinstional data/src/06ConditionalRendering/components/Expenses/ExpenseDate.js

---
+++ react/006 rendering lists and cinstional data/src/06ConditionalRendering/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2022, 5, 12),
+  },
+  {
+    id: "e21",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2022, 7, 14),
+  },
+  { id: "e22", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e23",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2019, 2, 28),
+  },
+  {
+    id: "e24",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2019, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(dummyExpenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
