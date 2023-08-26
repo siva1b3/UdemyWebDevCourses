404617b9d57822f26c7cc064e63b3c3b47071329
siva1b3 <siva1b3@gmail.com>
2023-08-26 21:39:25 +0530

    Changes to be committed: 	modified:   react/006 rendering lists and cinstional data/src/14AddingBarChart/App.js 	modified:   react/006 rendering lists and cinstional data/src/14AddingBarChart/components/Chart/MultipleTubes.css

--- react/006 rendering lists and cinstional data/src/14AddingBarChart/App.js
+++ react/006 rendering lists and cinstional data/src/14AddingBarChart/App.js
@@ -4,68 +4,129 @@ import MultipleTubes from "./components/Chart/MultipleTubes";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14), // August 14, 2020
-  },
-  {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12), // March 12, 2021
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28), // March 28, 2021
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12), // June 12, 2021
-  },
-  {
-    id: "e5",
-    title: "Groceries",
-    amount: 120.85,
-    date: new Date(2019, 0, 5), // January 5, 2019
-  },
-  {
-    id: "e6",
-    title: "Movie Tickets",
-    amount: 35.75,
-    date: new Date(2019, 7, 20), // August 20, 2019
-  },
-  {
-    id: "e7",
-    title: "Restaurant Dinner",
-    amount: 68.95,
-    date: new Date(2022, 10, 15), // November 15, 2022
-  },
-  {
-    id: "e8",
-    title: "Fitness Membership",
-    amount: 200,
-    date: new Date(2022, 2, 3), // March 3, 2022
-  },
-  {
-    id: "e9",
-    title: "Book Purchase",
-    amount: 25.5,
-    date: new Date(2022, 4, 18), // May 18, 2022
-  },
-  {
-    id: "e10",
-    title: "Home Repairs",
-    amount: 350,
-    date: new Date(2020, 6, 7), // July 7, 2020
-  },
+const DUMMY_EXPENSES = [];
+const possibleTitles = [
+  "Groceries",
+  "Entertainment",
+  "Shopping",
+  "Dining Out",
+  "Travel",
+  "Utilities",
+  "Healthcare",
+  "Education",
+  "Home Repairs",
+  "Subscriptions",
 ];
+
+for (let i = 1; i <= 100; i++) {
+  const id = `e${i}`;
+  const randomIndex = Math.floor(Math.random() * possibleTitles.length);
+  const amount = Math.floor(Math.random() * 5000) + 1;
+  const year = Math.floor(Math.random() * 4) + 2019;
+  const month = Math.floor(Math.random() * 12);
+  const day = Math.floor(Math.random() * 28) + 1;
+  const date = new Date(year, month, day);
+  const title = `${possibleTitles[randomIndex]} ${year} ${month} ${day}`;
+
+  DUMMY_EXPENSES.push({ id, title, amount, date });
+}
+
+// Assuming you already have the randomDataArray created from the previous example
+
+// Helper function to get the year and month as a string from a Date object
+function getYearMonthString(date) {
+  return `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+// Function to aggregate max, min, sum, and average expenses
+function aggregateExpensesForEachYearAndMonth(dataArray) {
+  const aggregatedData = {};
+
+  dataArray.forEach((data, index) => {
+    const yearMonth = getYearMonthString(data.date);
+
+    if (!aggregatedData[yearMonth]) {
+      aggregatedData[yearMonth] = {
+        year: data.date.getFullYear(),
+        month: data.date.getMonth() + 1,
+        expenses: [],
+      };
+    }
+
+    aggregatedData[yearMonth].expenses.push(data.amount);
+  });
+  return dataStatistics(aggregatedData);
+}
+
+function aggregateExpensesForonlyYear(dataArray) {
+  const aggregatedData = {};
+
+  dataArray.forEach((data, index) => {
+    const year = data.date.getFullYear().toString();
+
+    if (!aggregatedData[year]) {
+      aggregatedData[year] = {
+        year: data.date.getFullYear(),
+        expenses: [],
+      };
+    }
+
+    aggregatedData[year].expenses.push(data.amount);
+  });
+  return dataStatistics(aggregatedData);
+}
+
+function aggregateExpensesForonlyMonth(dataArray) {
+  const aggregatedData = {};
+
+  dataArray.forEach((data, index) => {
+    const month = data.date.getMonth().toString().padStart(2, "0");
+
+    if (!aggregatedData[month]) {
+      aggregatedData[month] = {
+        month: data.date.getMonth().toString().padStart(2, "0"),
+        expenses: [],
+      };
+    }
+
+    aggregatedData[month].expenses.push(data.amount);
+  });
+  return dataStatistics(aggregatedData);
+}
+
+function dataStatistics(data) {
+  for (const key in data) {
+    const expenses = data[key].expenses;
+    const maxExpense = Math.max(...expenses);
+    const minExpense = Math.min(...expenses);
+    const sumExpense = expenses.reduce((total, amount) => total + amount, 0);
+    const avgExpense = sumExpense / expenses.length;
+
+    data[key].maxExpense = maxExpense;
+    data[key].minExpense = minExpense;
+    data[key].sumExpense = sumExpense;
+    data[key].avgExpense = avgExpense;
+
+    delete data[key].expenses;
+  }
+
+  return data;
+}
+
+const aggregatedExpenses1 =
+  aggregateExpensesForEachYearAndMonth(DUMMY_EXPENSES);
+
+console.log(aggregatedExpenses1);
+
+const aggregatedExpenses2 = aggregateExpensesForonlyYear(DUMMY_EXPENSES);
+
+console.log(aggregatedExpenses2);
+
+const aggregatedExpenses3 = aggregateExpensesForonlyMonth(DUMMY_EXPENSES);
+
+console.log(aggregatedExpenses3);
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
