799b44d8627bdc7c926e9009de613fb4df7f226b
siva1b3 <siva1b3@gmail.com>
2023-08-27 08:35:43 +0530

    Changes to be committed: 	modified:   react/07Styling/src/02IineStylesConditional/components/CourseGoals/CourseInput/CourseInput.js 	modified:   react/07Styling/src/index.js

--- react/07Styling/src/index.js
+++ react/07Styling/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 
 import "./index.css";
-import App01 from "./01Startercode/App";
+// import App01 from "./01Startercode/App";
+import App02 from "./02IineStylesConditional/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     {/* starter code  */}
-    <App01 />
+    {/* <App01 /> */}
+
+    {/* Conditional Inline styles */}
+    <App02 />
   </StrictMode>
 );
