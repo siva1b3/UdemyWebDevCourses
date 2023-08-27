41b3a47f11d11081a74a59f6727f5efca01a03f8
siva1b3 <siva1b3@gmail.com>
2023-08-27 07:50:05 +0530

    Changes to be committed: 	renamed:    react/07Styling/src/01/App.css -> react/07Styling/src/01Startercode/App.css 	renamed:    react/07Styling/src/01/App.js -> react/07Styling/src/01Startercode/App.js 	renamed:    react/07Styling/src/01/components/CourseGoals/CourseGoalItem/CourseGoalItem.css -> react/07Styling/src/01Startercode/components/CourseGoals/CourseGoalItem/CourseGoalItem.css

--- react/07Styling/src/index.js
+++ react/07Styling/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App01 from "./01Startercode/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    {/* starter code  */}
+    <App01 />
+  </StrictMode>
+);
