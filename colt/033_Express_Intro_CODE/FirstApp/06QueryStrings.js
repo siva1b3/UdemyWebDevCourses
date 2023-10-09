be70806e92e85759aa158efcae9181ffbfb7d842
siva1b3 <siva1b3@gmail.com>
2023-10-09 11:00:36 +0530

    Changes to be committed: 	new file:   colt/033_Express_Intro_CODE/FirstApp/06QueryStrings.js 	new file:   unnessary/Private Sub Workbook_Open().txt 	new file:   unnessary/text main.txt

---
+++ colt/033_Express_Intro_CODE/FirstApp/06QueryStrings.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// app.use((incommingRequest, outgoingResponse) => {
+//   console.log("WE GOT A NEW REQUEST!!");
+//   outgoingResponse.send("<h1 style='color:red;'>This is my web page</h1>");
+// });
+
+app.get("/siva/:parameter1", function (incommingRequest, outgoingResponse) {
+  const { parameter1 } = incommingRequest.params;
+  console.log(parameter1);
+  outgoingResponse.send(
+    `<h1 style='color:red;'>This is Page of ${parameter1}</h1>`
+  );
+});
+
+app.get(
+  "/siva/:parameter1/:parameter2",
+  function (incommingRequest, outgoingResponse) {
+    const { parameter1, parameter2 } = incommingRequest.params;
+    console.log(parameter1, parameter2);
+    outgoingResponse.send(
+      `<h1 style='color:green;'>This is Page of ${parameter1} 
+      and subpage of ${parameter2}</h1>`
+    );
+  }
+);
+
+app.get("/cat", function (incommingRequest, outgoingResponse) {
+  outgoingResponse.send(
+    "<h1 style='color:red;'>This is my web page of a cat</h1>"
+  );
+});
+
+app.get("/dog", function (incommingRequest, outgoingResponse) {
+  outgoingResponse.send(
+    "<h1 style='color:red;'>This is my web page of a dog</h1>"
+  );
+});
+
+app.get("/", function (incommingRequest, outgoingResponse) {
+  outgoingResponse.send("<h1 style='color:red;'>This is Home web page </h1>");
+});
+
+app.post("/cat", function (incommingRequest, outgoingResponse) {
+  outgoingResponse.send("<h1 style='color:red;'>This is a post requet</h1>");
+});
+
+app.get("*", function (incommingRequest, outgoingResponse) {
+  outgoingResponse.send("<h1 style='color:red;'>This is Wrong stop it</h1>");
+});
+
+app.listen(port, () => {
+  console.log("LISTENING ON PORT 8080");
+});
