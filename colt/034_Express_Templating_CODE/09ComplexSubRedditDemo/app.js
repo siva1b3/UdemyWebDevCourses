7126b5b7b62b09d03a3e3673cf78e5e6ff5a612d
siva1b3 <siva1b3@gmail.com>
2023-10-21 23:53:54 +0530

    Changes to be committed: 	modified:   colt/034_Express_Templating_CODE/09ComplexSubRedditDemo/app.js 	new file:   colt/034_Express_Templating_CODE/09ComplexSubRedditDemo/public/app.css 	modified:   colt/034_Express_Templating_CODE/09views/ComplexReddit.ejs 	modified:   colt/034_Express_Templating_CODE/09views/notFound.ejs 	new file:   colt/034_Express_Templating_CODE/10PartialTemplates/public/.DS_Store 	new file:   colt/034_Express_Templating_CODE/10PartialTemplates/public/css/.DS_Store 	new file:   colt/034_Express_Templating_CODE/10PartialTemplates/public/css/bootstrap.min.css 	new file:   colt/034_Express_Templating_CODE/10PartialTemplates/public/js/.DS_Store 	new file:   colt/034_Express_Templating_CODE/10PartialTemplates/public/js/bootstrap.min.js 	new file:   colt/034_Express_Templating_CODE/10PartialTemplates/public/js/jquery.js

--- colt/034_Express_Templating_CODE/09ComplexSubRedditDemo/app.js
+++ colt/034_Express_Templating_CODE/09ComplexSubRedditDemo/app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 const parentDirectory = path.join(__dirname, "..");
 app.set("views", path.join(parentDirectory, "09views"));
