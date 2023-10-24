bf3909ff77ce4a6fd7f04f9c26f415b30f76b58c
siva1b3 <siva1b3@gmail.com>
2023-10-24 11:22:14 +0530

    Changes to be committed: 	modified:   colt/035_RESTful_Routes_CODE/03RestfulApi/data/data.json 	modified:   colt/035_RESTful_Routes_CODE/03RestfulApi/routes.js 	modified:   colt/035_RESTful_Routes_CODE/03RestfulApi/routes/getComments.js

--- colt/035_RESTful_Routes_CODE/03RestfulApi/routes.js
+++ colt/035_RESTful_Routes_CODE/03RestfulApi/routes.js
@@ -1,2 +1,34 @@
+const { express, app, port } = require("./app");
+const jsonData = require("./data/data.json");
+const comments = jsonData["main"];
+const { v4: uuid } = require("uuid"); //For generating ID's
+
 require("./routes/getRootRoute");
 require("./routes/getComments");
+
+// **********************************
+// NEW - renders a form
+// **********************************
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new.ejs");
+});
+
+// **********************************
+// CREATE - creates a new comment
+// **********************************
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment, id: uuid() });
+  console.log(comments);
+  // res.send("hi");
+  res.redirect("/comments");
+});
+
+// *******************************************
+// SHOW - details about one particular comment
+// *******************************************
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
--- colt/035_RESTful_Routes_CODE/03RestfulApi/routes/getComments.js
+++ colt/035_RESTful_Routes_CODE/03RestfulApi/routes/getComments.js
@@ -2,6 +2,9 @@ const { app, express } = require("../app");
 const JsonData = require("../data/data.json");
 const comments = JsonData["main"];
 
+// **********************************
+// INDEX - renders multiple comments
+// **********************************
 app.get("/comments", function (req, res) {
   res.render("comments/index.ejs", { comments });
 });
